820f5346f85dd8dd28a9aaaadb6efc7a19cceaef
ydvkrn <kyadav00011199@gmail.com>
2025-03-21 14:42:44 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+{
+  "content": {
+    "backgroundImage": "https://i.giphy.com/l2QE5bDTfjOKHmme4.webp",
+    "warningImage": "https://i.postimg.cc/Njbkc17s/Zqz-Bsh5-Le-NNTxw-WY-newsletter-risk1-gif-498-310.gif",
+    "welcomeText": "WELCOME TO",
+    "mainTitle": "MSM WORLD!",
+    "subtitle": "Bad Thought Forever",
+    "audio": "https://res.cloudinary.com/dohrsxrnr/video/upload/v1742392492/anlsbdkjmip2c8qqjaoq.mp3",
+    "mediaItems": [
+     {
+            "name": "SUPREME",
+            "link": "supreme.html",
+            "media": "https://res.cloudinary.com/dohrsxrnr/video/upload/v1740477128/tpvbvyrzteujpgtkmu4b.mp4"
+        },
+     {
+        "name": "INSPIRATION",
+        "media": "https://res.cloudinary.com/dohrsxrnr/video/upload/v1740520243/ruclee2kzgtrf0vquqes.mp4",
+        "link": "inspire.html"
+      },
+      {
+        "name": "FAVOURITE",
+        "media": "https://i.postimg.cc/x1r83Fk3/billieeilish-20250319-0002.jpg",
+        "link": "fav.html"
+      },
+{
+            "name": "LIFE UPDATE",
+            "link": "life.html",
+            "media": "https://res.cloudinary.com/dohrsxrnr/video/upload/v1742394360/sppjja2lgfc7bwm3x380.mp4"
+        },
+      {
+        "name": "Elon Musk",
+        "media": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/330px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
+        "link": "https://en.wikipedia.org/wiki/Elon_Musk"
+      }
+    ]
+  }
+}
